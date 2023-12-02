3861c9745557c5882c9d1b71bc03722f32bea9ff
user <you@example.com>
2023-12-02 13:48:35 +0500

    02.12.2023

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,21 +1,3 @@
-/*#include <stdio.h>
-#include <math.h>
-
-int main() 
-{
-    float x,f,h;
-    x=0.0;
-scanf("%f",&h);
-while(x<2+h/2){
-if(x>2)
-x=2;
-if (x<=1)
- f=cos(x)*exp(-x*x);
-else
-f=log(x+1)-sqrt(4-x*x);
-printf("%f %f\n",x,f);
-x=x+h;}
-}*/
 
 #include <stdio.h>
 #include <math.h>
@@ -25,7 +7,7 @@ int main()
     float x,f,h;
     x=0.0;
 scanf("%f",&h);
-for(x;x < (2+(h/2));h)
+while(x<2+h/2)
 {
 if(x>2)
 x=2;
@@ -34,5 +16,26 @@ if (x<=1)
 else
 f=log(x+1)-sqrt(4-x*x);
 printf("%f %f\n",x,f);
+x=x+h;}
+}
+
+
+#include <stdio.h>
+#include <math.h>
+
+int main() 
+{
+    float x,f,h;
+    x=0.0;
+scanf("%f",&h); 
+for(x; x<(2+(h/2));h)
+{
+if(x>2) 
+x=2;
+if (x<=1) 
+ f=cos(x)*exp(-x*x);
+else 
+f=log(x+1)-sqrt(4-x*x);
+printf("%f %f\n",x,f); 
 x=x+h;}
 }
